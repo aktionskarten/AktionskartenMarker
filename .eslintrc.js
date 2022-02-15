32858d33bd19fd55fe60f29d77d6fc3f37c6d117
kohllisti <kohllisti@riseup.net>
2022-02-15 21:31:01 +0100

    Update eslint and use airbnb style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,14 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
-    },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
 };
--- AktionskartenMarker.js
+++ AktionskartenMarker.js
@@ -5,61 +5,62 @@
  */
 L.StyleEditor.marker.AktionskartenMarker = L.StyleEditor.marker.Marker.extend({
 
-    createMarkerIcon: function (iconOptions) {
-        var size = iconOptions.iconSize;
-        var icon = iconOptions.icon;
-        var color = iconOptions.iconColor;
+  /* eslint-disable new-cap */
+  createMarkerIcon(iconOptions) {
+    const size = iconOptions.iconSize;
+    const { icon } = iconOptions;
+    const color = iconOptions.iconColor;
 
-        return new L.divIcon({
-            className: 'leaflet-styleeditor-aktionsmarker-marker' + '-' + this.sizeToName(size),
-            icon: icon,
-            bgPos: this._backgroundPosition(color, icon, size),
-            iconColor: color,
-            iconSize: size,
-            iconAnchor: [size[0] / 2, size[1] / 2],
-            popupAnchor: [0, 0]
-        });
-    },
+    return new L.divIcon({
+      className: `leaflet-styleeditor-aktionsmarker-marker-${this.sizeToName(size)}`,
+      icon,
+      bgPos: this.backgroundPosition(color, icon, size),
+      iconColor: color,
+      iconSize: size,
+      iconAnchor: [size[0] / 2, size[1] / 2],
+      popupAnchor: [0, 0],
+    });
+  },
 
-    createSelectHTML: function (parentUiElement, iconOptions, icon) {
-        var tmpOptions = {};
-        tmpOptions.iconSize = this.options.size.medium;
-        tmpOptions.icon = icon;
-        tmpOptions.iconColor = iconOptions.iconColor;
+  /* eslint-disable no-param-reassign */
+  createSelectHTML(parentUiElement, iconOptions, icon) {
+    const tmpOptions = {};
+    tmpOptions.iconSize = this.options.size.medium;
+    tmpOptions.icon = icon;
+    tmpOptions.iconColor = iconOptions.iconColor;
 
-        parentUiElement.innerHTML = this.createMarkerIcon(tmpOptions).createIcon().outerHTML;
-    },
+    parentUiElement.innerHTML = this.createMarkerIcon(tmpOptions).createIcon().outerHTML;
+  },
 
-    _backgroundPosition: function(color, icon, size) {
-        color = this.options.styleEditorOptions.util.rgbToHex(color);
-        size = this.sizeToPixel(size);
+  backgroundPosition(color, icon, size) {
+    const hexColor = this.options.styleEditorOptions.util.rgbToHex(color);
+    const pixelSize = this.sizeToPixel(size);
 
-        var row = this.options.colorRamp.indexOf(color);
-        var colorIcons = this.options.markers[color];
-        if (typeof colorIcons === 'undefined') {
-            colorIcons = this.options.markers.default;
-        }
-        var col = colorIcons.indexOf(icon);
-        return L.point(col*size[0], row*size[1]);
-    },
-
-    options: {
-        size: {
-            'small': [20, 20],
-            'medium': [30, 30],
-            'large': [40, 40]
-        },
-
-        colorRamp: [
-            '#e04f9e', '#fe0000', '#ee9c00', '#ffff00', '#00e13c', '#00a54c', '#00adf0', '#7e55fc', '#1f4199', '#7d3411'
-        ],
-
-        markers: {
-            'default': ['train', 'megaphone', 'tent', 'speaker', '?', 'cooking-pot', 'police', 'nuclear', 'empty',
-                'point', 'information', 'exclamation-mark', 'star', 'star-megaphone', 'arrow', 'bang'],
-            '#7d3411': ['flag', 'megaphone', 'empty', 'point', 'exclamation-mark', 'thor-steinar', 'arrow']
-        }
+    const row = this.options.colorRamp.indexOf(hexColor);
+    let colorIcons = this.options.markers[hexColor];
+    if (typeof colorIcons === 'undefined') {
+      colorIcons = this.options.markers.default;
     }
+    const col = colorIcons.indexOf(icon);
+    return L.point(col * pixelSize[0], row * pixelSize[1]);
+  },
 
+  options: {
+    size: {
+      small: [20, 20],
+      medium: [30, 30],
+      large: [40, 40],
+    },
+
+    colorRamp: [
+      '#e04f9e', '#fe0000', '#ee9c00', '#ffff00', '#00e13c', '#00a54c', '#00adf0', '#7e55fc', '#1f4199', '#7d3411',
+    ],
+
+    markers: {
+      default: ['train', 'megaphone', 'tent', 'speaker', '?', 'cooking-pot', 'police', 'nuclear', 'empty',
+        'point', 'information', 'exclamation-mark', 'star', 'star-megaphone', 'arrow', 'bang'],
+      '#7d3411': ['flag', 'megaphone', 'empty', 'point', 'exclamation-mark', 'thor-steinar', 'arrow'],
+    },
+  },
 
 });
